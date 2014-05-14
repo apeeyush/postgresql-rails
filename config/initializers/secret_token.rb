943c29d1fd672c2b202d1fe20865568b595af396
peeyush <peeyushagarwal1994@gmail.com>
2014-05-14 17:01:13 +0530

    Added an article scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostgresTest::Application.config.secret_key_base = 'f11b3c66898487ec4b4702f575dbec6a0a034b95c064c6713dd3645bc698c9689cb1d756823247f9378713326c1e9138d901776b81dab1152279548e289a14aa'
